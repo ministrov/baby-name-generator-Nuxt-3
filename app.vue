5ca953946f9e5669bb4fc398e0347389a5bf0ff5
ministrov <61511030+ministrov@users.noreply.github.com>
2023-01-19 18:08:13 +0300

    feat: add the main state and transform it properly

--- app.vue
+++ app.vue
@@ -6,29 +6,108 @@
       <div class="option-container">
         <h4>1 Choose a gender</h4>
         <div class="option-button">
-          <button class="option option-left">Boy</button>
-          <button class="option">Unisex</button>
-          <button class="option option-right">Girl</button>
+          <button class="option option-left"
+          :class="options.gender === Gender.BOY && 'option-active'"
+          @click="options.gender = Gender.BOY"
+          >Boy
+          </button>
+          <button class="option"
+          :class="options.gender === Gender.UNISEX && 'option-active'"
+          @click="options.gender = Gender.UNISEX"
+          >Unisex
+          </button>
+          <button class="option option-right"
+          :class="options.gender === Gender.GIRL && 'option-active'"
+          @click="options.gender = Gender.GIRL"
+          >Girl
+          </button>
         </div>
       </div>
       <div class="option-container">
         <h4>2 Choose a name's popularity</h4>
         <div class="option-button">
-          <button class="option option-left">Trendy</button>
-          <button class="option option-right">Unique</button>
+          <button class="option option-left"
+          :class="options.popularity === Popularity.TRENDY && 'option-active'"
+          @click="options.popularity = Popularity.TRENDY"
+          >Trendy
+          </button>
+          <button class="option option-right"
+          :class="options.popularity === Popularity.UNIQUE && 'option-active'"
+          @click="options.popularity = Popularity.UNIQUE"
+          >Unique
+          </button>
         </div>
       </div>
       <div class="option-container">
         <h4>3 Choose a name's length</h4>
         <div class="option-button">
-          <button class="option option-left">Long</button>
-          <button class="option">All</button>
-          <button class="option option-right">Short</button>
+          <button class="option option-left"
+          :class="options.length === Length.LONG && 'option-active'"
+          @click="options.length = Length.LONG"
+          >Long
+          </button>
+          <button class="option"
+          :class="options.length === Length.ALL && 'option-active'"
+          @click="options.length = Length.ALL"
+          >All
+          </button>
+          <button class="option option-right"
+          :class="options.length === Length.SHORT && 'option-active'"
+          @click="options.length = Length.SHORT"
+          >Short
+          </button>
         </div>
+      </div>
+      <button class="primary"
+      @click="computeSelectedNames"
+      >Find Names
+      </button>
+    </div>
+    <div class="cards-container">
+      <div v-for="name in selectedNames" key="name" class="card">
+        <h4>{{ name }}</h4>
+        <p>x</p>
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Gender, Popularity, Length, names } from '@/data';
+
+interface OptionsState {
+  gender: Gender,
+  popularity: Popularity,
+  length: Length
+}
+
+const obj: OptionsState = {
+  gender: Gender.GIRL,
+  popularity: Popularity.TRENDY,
+  length: Length.ALL
+}
+
+const options: OptionsState = reactive({
+  gender: Gender.GIRL,
+  popularity: Popularity.TRENDY,
+  length: Length.ALL
+});
+
+const computeSelectedNames = () => {
+  const filteredNames = names
+    .filter((name) => name.gender === options.gender)
+    .filter((name) => name.popularity === options.popularity)
+    .filter((name) => {
+      if (options.length === Length.ALL) return true;
+      else return name.length === options.length;
+    })
+
+    selectedNames.value = filteredNames.map((name) => name.name);
+}
+
+const selectedNames = ref<string[]>([]);
+
+</script>
 
 <style scoped>
 .container {
@@ -77,4 +156,46 @@ h1 {
 .option-right {
   border-radius: 0 1rem 1rem 0;
 }
+
+.option-active {
+  background-color: rgb(249, 87, 89);
+  color: white;
+}
+
+.primary {
+  margin-top: 2.2rem;
+  padding: 0.75rem 4rem;
+  font-size: 1rem;
+  color: white;
+  background-color: rgb(249, 87, 89);
+  border: none;
+  border-radius: 6.5rem;
+  cursor: pointer;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card {
+  position: relative;
+  width: 28%;
+  margin-right: 0.4rem;
+  margin-bottom: 1rem;
+  padding: 0.1rem;
+  color: white;
+  background-color: rgb(27, 60, 138);
+  border-radius: 1rem;
+}
+
+.card p {
+  position: absolute;
+  top: -17%;
+  left: 91.5%;
+  color: rgba(255, 255, 255, 0.178);
+  cursor: pointer;
+}
+
+/* 200 lines of code */
 </style>
